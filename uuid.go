148051fd6be86da16a462d5d15c6ebdd89da6633
vycb <vycb777@gmail.com>
2018-11-11 17:21:49 +0200

    v2.0

--- uuid.go
+++ uuid.go
@@ -1,13 +1,21 @@
 //
 //  libuuid for golang by cgo
 //
-//  library install for debian
+//  library install for ubuntu, debian
 //      $ sudo apt install uuid-dev
-//  By default uuid_generate_time_safe is used.
-//  Also uuid_generate can be enabled, which is slower, by uncommenting #define USEDEVREND 1
+//  
+//  Usage:
+//  oUuid = NewGen()
+//  defer oUuid.Close()
+
+//  if oUuid.IsNull() {
+//    t.Error("Error to initialize NewGen")
+//  }
+//  log.Printf("NewGen UnparseUpper:%s\n", oUuid.UnparseUpper())
+//
 package uuid
 /*
-#cgo CFLAGS: -I/usr/include/uuid -pipe -Wall -O3 -fomit-frame-pointer -march=native -fopenmp -D_FILE_OFFSET_BITS=64
+#cgo CFLAGS: -I/usr/include/uuid -pipe -Wall -O3 -Wint-conversion -fomit-frame-pointer -march=native -fopenmp -D_FILE_OFFSET_BITS=64
 #cgo LDFLAGS: -luuid
 #include <uuid.h>
 #include <stdlib.h>
@@ -16,81 +24,321 @@ package uuid
 
 ////#define USEDEVREND 1
 
+//unsigned char uuid[16];
+//unsigned char uuid2[16];
 uuid_t uuid;
 uuid_t uuid2;
 
-static void getuuidlower(char* uuid_str) {
-#ifdef USEDEVREND
+static inline void getuuidlower(char* uuid_str) {
+//{{{
+#ifdef ifdefUSEDEVREND
 	uuid_generate(uuid);
 #else
 	uuid_generate_time_safe(uuid);
 #endif
 	uuid_unparse_lower(uuid, uuid_str);
-}
+} //}}}
 
-static void getuuidupper(char* uuid_str) {
+static inline void getuuidupper(char* uuid_str) {
+//{{{
 #ifdef USEDEVREND
 	uuid_generate(uuid);
 #else
 	uuid_generate_time_safe(uuid);
 #endif
 	uuid_unparse_upper(uuid, uuid_str);
-}
+} //}}}
 
-static void getuuidmd5upper(char* uuid_str, char *name) {
-	uuid_parse(uuid_str, uuid2);
+static inline void getuuidmd5upper(char* uuid_str, char *name) {
+//{{{
+	uuid_t uuidl;
+	uuid_t uuidl2;
+	uuid_parse(uuid_str, uuidl);
+	uuid_generate_md5(uuidl2, uuidl, name, strlen(name));
+	uuid_unparse_upper(uuidl2, uuid_str);
+	//printf("getuuidmd5upper: %s\n", uuid_str);
+} //}}}
 
-	uuid_generate_md5(uuid, uuid2, name, strlen(name));
+static inline void getuuidmd5lower(char* uuid_str, char *name) {
+//{{{
+	uuid_t uuidl;
+	uuid_t uuidl2;
+	uuid_parse(uuid_str, uuidl);
+	uuid_generate_md5(uuidl2, uuidl, name, strlen(name));
+	uuid_unparse_lower(uuidl2, uuid_str);
+	//printf("getuuidmd5lower: %s\n", uuid_str);
+} //}}}
 
-	uuid_unparse_upper(uuid, uuid_str);
-}
+static inline void genuuidmd5(uuid_t uuida, char* uuid_str, char *name) {
+//{{{
+	uuid_t uuidl;
+	uuid_parse(uuid_str, uuidl);
+	uuid_generate_md5(uuida, uuidl, name, strlen(name));
+	//char uuid_stp[37];
+	//uuid_unparse_upper(uuida, uuid_stp);
+	//printf("genuuidmd5: %s\n", uuid_stp);
+} //}}}
 
-static void getuuidmd5lower(char* uuid_str, char *name) {
-	uuid_parse(uuid_str, uuid2);
-
-	uuid_generate_md5(uuid, uuid2, name, strlen(name));
-
-	uuid_unparse_lower(uuid, uuid_str);
-}
+static void genuuidsha1(unsigned char* uuida, char* uuid_str, char *name) {
+//{{{
+	uuid_t uuidl;
+	uuid_parse(uuid_str, uuidl);
+	uuid_generate_sha1(uuida, uuidl, name, strlen(name));
+	//char uuid_stp[37];
+	//uuid_unparse_upper(uuida, uuid_stp);
+	//printf("genuuidsha1: %s\n", uuid_stp);
+} //}}}
 
 static void getuuidsha1upper(char* uuid_str, char *name) {
+//{{{
+	uuid_t uuidl;
+	uuid_t uuidl2;
+	uuid_parse(uuid_str, uuidl);
+	uuid_generate_sha1(uuidl2, uuidl, name, strlen(name));
+	uuid_unparse_upper(uuidl2, uuid_str);
+	//printf("getuuidsha1upper: %s\n", uuid_str);
+} //}}}
+
+static inline void getuuidsha1lower(char* uuid_str, char *name) {
+//{{{
 	uuid_parse(uuid_str, uuid2);
-
 	uuid_generate_sha1(uuid, uuid2, name, strlen(name));
-
-	uuid_unparse_upper(uuid, uuid_str);
-}
-
-static void getuuidsha1lower(char* uuid_str, char *name) {
-	uuid_parse(uuid_str, uuid2);
-
-	uuid_generate_sha1(uuid, uuid2, name, strlen(name));
-
 	uuid_unparse_lower(uuid, uuid_str);
-}
+	//printf("getuuidsha1lower: %s\n", uuid_str);
+} //}}}
 
-static int compareuuid(char* uuid_str1, char* uuid_str2){
+static inline int compareuuid(char* uuid_str1, char* uuid_str2){
+//{{{
 	uuid_parse(uuid_str1, uuid);
 	uuid_parse(uuid_str2, uuid2);
 	return uuid_compare(uuid, uuid2);
-}
+} //}}}
 
 */
 import "C"
 import "unsafe"
 //import "log"
-var uuid_byte [37]byte
+const (
+	// uuid_t u_char[16]
+	UTY = 16
+	// UUID_STR_LEN
+	USL = 37
+)
+var uuid_byte [USL]byte
 
-func GetUUIDUpper() string{
+type UUID struct{
+	//uuid unsafe.Pointer
+	uuidb []byte
+	val string //*C.char
+}
+
+// use: defer oUuid.Close()
+//
+func (u *UUID) Close() {
+	//C.free(unsafe.Pointer(u.uuid))
+	var s []byte
+	u.uuidb = s
+}
+
+// uuid_is_null(uuid_t uuid)
+// clear.c
+func (u *UUID) Clear() {
+	//C.uuid_clear((*C.u_char)(u.uuid))
+	u.Close()
+}
+
+// get: Val()
+//
+func (u *UUID) Val() string{
+	if len(u.val) == 0{
+		u.val = u.UnparseUpper()
+	}
+	return u.val
+}
+
+// NewGenMd5
+// Generate an MD5 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
+func NewGenMd5(uuid, sname string) *UUID{
+	//var bup [16]byte
+	//bup := make([]byte, 16)
+	up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+  uuid_str := C.CString(uuid)
+	defer C.free(unsafe.Pointer(uuid_str))
+	ustrn := C.CString(sname)
+	defer C.free(unsafe.Pointer(ustrn))
+
+	C.genuuidmd5((*C.u_char)(up), uuid_str, ustrn)
+	ubyt := C.GoBytes(up, C.int(UTY))
+
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// NewGenSha1
+// Generate a SHA1 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
+func NewGenSha1(uuid, sname string) *UUID{
+	//var bup [16]byte
+	//bup := make([]byte, 16, 16)
+	//var up unsafe.Pointer
+	up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+	var uuid_str *C.char
+	uuid_str = C.CString(uuid)
+	defer C.free(unsafe.Pointer(uuid_str))
+
+	//var ustrn *C.char
+	ustrn := C.CString(sname)
+	defer C.free(unsafe.Pointer(ustrn))
+
+	//puch := (unsafe.Pointer)(C.genuuidsha1((*C.u_char)(up), uuid_str, ustrn))
+	//ubyt := C.GoBytes(puch, C.int(16))
+	C.genuuidsha1((*C.u_char)(up), uuid_str, ustrn)
+	ubyt := C.GoBytes(up, C.int(UTY))
+
+	//C.uuid_generate_sha1((*C.u_char)(up), (*C.u_char)(tp), ustrn, (C.ulong)(len(sname)))
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// uuid_generate_time(uuid_t out)
+// gen_uuid.c
+func NewGenTime() *UUID{
+	//var bup [16]byte
+  up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+	C.uuid_generate_time((*C.u_char)(up))
+	ubyt := C.GoBytes(up, C.int(UTY))
+
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// uuid_generate_time_safe(uuid_t out)
+// gen_uuid.c
+func NewGenTimeSafe() *UUID{
+	//var bup [16]byte
+  up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+	C.uuid_generate_time_safe((*C.u_char)(up))
+	ubyt := C.GoBytes(up, C.int(UTY))
+
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// uuid_generate(uuid_t out)
+// gen_uuid.c
+func NewGen() *UUID{
+	//var bup [16]byte
+  up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+	C.uuid_generate((*C.u_char)(up))
+	ubyt := C.GoBytes(up, C.int(UTY))
+	//log.Printf("NewGen:ubyt: len:%v, cap:%v", len(ubyt), cap(ubyt))
+
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// uuid_generate_random(uuid_t out)
+// gen_uuid.c
+func NewGenRandom() *UUID{
+	//var bup [16]byte
+  up := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(up))
+
+	//defer C.free(unsafe.Pointer(up))
+	C.uuid_generate_random((*C.u_char)(up))
+	ubyt := C.GoBytes(up, C.int(UTY))
+	//log.Printf("NewGenRandom:ubyt: len:%v, cap:%v", len(ubyt), cap(ubyt))
+
+	return &UUID{uuidb: ubyt, val: ""}
+}
+
+// uuid_is_null(uuid_t uuid)
+// isnull.c
+func (u *UUID) IsNull() bool{
+	//res := int(C.uuid_is_null((*C.u_char)(u.uuid)))
+	//log.Printf("IsNull:uuidb: len:%v, cap:%v", len(u.uuidb), cap(u.uuidb))
+
+	if len(u.uuidb) == 0{
+		return true
+	}
+	return false
+}
+
+// uuid_compare(uuid_t uuid1, uuid_t uuid2)
+// compare.c, parse.c
+func (u *UUID) Compare(uid string) int{
+	uuid_str := C.CString(uid)
+	defer C.free(unsafe.Pointer(uuid_str))
+	//var bup [16]byte
+  nup := C.CBytes(uuid_byte[:UTY])
+	defer C.free(unsafe.Pointer(nup))
+	C.uuid_parse(uuid_str, (*C.u_char)(nup))
+
+  up := C.CBytes(u.uuidb)
+	defer C.free(unsafe.Pointer(up))
+	return int(C.uuid_compare((*C.u_char)(up), (*C.u_char)(nup)))
+}
+
+// uuid_unparse_lower(uuid_t out)
+// gen_uuid.c
+func (u *UUID) UnparseLower() string{
+	//var bup [37]byte
+	bs := string(uuid_byte[:USL])
+	uuid_str := C.CString(bs)
+	defer C.free(unsafe.Pointer(uuid_str))
+
+	up := C.CBytes(u.uuidb)
+  defer C.free(unsafe.Pointer(up))
+	C.uuid_unparse_lower((*C.u_char)(up), uuid_str)
+	u.val = C.GoString(uuid_str)
+	return u.val
+}
+
+// uuid_unparse_upper(uuid_t out)
+// gen_uuid.c
+func (u *UUID) UnparseUpper() string{
 	bs := string(uuid_byte[:37])
+	uuid_str := C.CString(bs)
+	defer C.free(unsafe.Pointer(uuid_str))
+	//up := C.CBytes(uuid_byte[:16])
+	//up := string(uuid_byte[:16])
+	//uuid := C.CString(up)
+	//defer C.free(unsafe.Pointer(up))
+	//C.uuid_generate_time_safe((*C.u_char)(up))
+	//if(len(u.uuidb)){
+	//}
+	up := C.CBytes(u.uuidb)
+	defer C.free(unsafe.Pointer(up))
+
+	C.uuid_unparse_upper((*C.u_char)(up), uuid_str)
+	u.val = C.GoString(uuid_str)
+	return u.val
+}
+
+// util: GetUUIDUpper
+// uuid_generate_time_safe
+func GetUUIDUpper() string{
+	//var bup [37]byte
+	bs := string(uuid_byte[:USL])
 	uuid_str := C.CString(bs)
 	defer C.free(unsafe.Pointer(uuid_str))
 	C.getuuidupper(uuid_str)
 	return C.GoString(uuid_str)
 }
 
+// util: GetUUIDLower
+// uuid_generate_time_safe
 func GetUUIDLower() string{
-	bs := string(uuid_byte[:37])
+	//var bup [37]byte
+	bs := string(uuid_byte[:USL])
 	uuid_str := C.CString(bs)
 	defer C.free(unsafe.Pointer(uuid_str))
 	C.getuuidlower(uuid_str)
@@ -98,6 +346,8 @@ func GetUUIDLower() string{
 	return C.GoString(uuid_str)
 }
 
+// util: UUIDCompare u1, u2 string
+//
 func UUIDCompare(uuid_str1, uuid_str2 string) int{
 	ustr1 := C.CString(uuid_str1)
   defer C.free(unsafe.Pointer(ustr1))
@@ -106,6 +356,10 @@ func UUIDCompare(uuid_str1, uuid_str2 string) int{
 	return int(C.compareuuid(ustr1, ustr2))
 }
 
+// util: GetUUIDMd5Upper
+// Generate an MD5 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
 func GetUUIDMd5Upper(uuid, name string) string {
   uuid_str := C.CString(uuid)
 	defer C.free(unsafe.Pointer(uuid_str))
@@ -115,6 +369,10 @@ func GetUUIDMd5Upper(uuid, name string) string {
 	return C.GoString(uuid_str)
 }
 
+// util: GetUUIDMd5Lower
+// Generate an MD5 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
 func GetUUIDMd5Lower(uuid, name string) string {
   uuid_str := C.CString(uuid)
 	defer C.free(unsafe.Pointer(uuid_str))
@@ -124,10 +382,10 @@ func GetUUIDMd5Lower(uuid, name string) string {
 	return C.GoString(uuid_str)
 }
 
-/*
- * Generate a SHA1 hashed (predictable) UUID based on a well-known UUID
- * providing the namespace and an arbitrary binary string.
- */
+// util: GetUUIDSha1Lower
+// Generate a SHA1 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
 func GetUUIDSha1Lower(uuid, name string) string {
   uuid_str := C.CString(uuid)
 	defer C.free(unsafe.Pointer(uuid_str))
@@ -137,10 +395,10 @@ func GetUUIDSha1Lower(uuid, name string) string {
 	return C.GoString(uuid_str)
 }
 
-/*
- * Generate a SHA1 hashed (predictable) UUID based on a well-known UUID
- * providing the namespace and an arbitrary binary string.
- */
+// util: GetUUIDSha1Upper
+// Generate a SHA1 hashed (predictable) UUID based on a well-known UUID
+// providing the namespace and an arbitrary binary string.
+//
 func GetUUIDSha1Upper(uuid, name string) string {
   uuid_str := C.CString(uuid)
 	defer C.free(unsafe.Pointer(uuid_str))
--- uuid_test.go
+++ uuid_test.go
@@ -1,8 +1,8 @@
 //
 //  libuuid sample program
 //
-//  library install for debian
-//      $ sudo apt-get install uuid-dev
+//  library install for ubuntu, debian
+//      $ sudo apt install uuid-dev
 //
 //
 package uuid
@@ -11,14 +11,85 @@ import (
 	"testing"
 	"log"
 	"github.com/jakehl/goid"
+	gouuid "github.com/satori/go.uuid"
 )
 var(
 	uuid1, uuid2, v4UUID string
+	oUuid *UUID
 )
+
+func TestNewGenRandom(t *testing.T) {
+	oUuid = NewGenRandom()
+	defer oUuid.Close()
+	if oUuid.IsNull() {
+		t.Error("Error to initialize NewGenRandom")
+	}
+	log.Printf("NewGenRandom UnparseUpper:%s\n", oUuid.UnparseUpper())
+	if len(oUuid.Val()) != 36 {
+		t.Error("Error in UnparseUpper")
+	}
+	uuid2 = oUuid.Val()
+	log.Printf("NewGenRandom UnparseLower:%s\n", oUuid.UnparseLower())
+	if len(oUuid.Val()) != 36 {
+		t.Error("Error in UnparseLower()")
+	}
+	res:=oUuid.Compare(uuid2)
+	if res != 0  {
+		t.Error("Error to Compare() 1 with UnparseUpper() in NewGenRandom")
+	}else{
+		log.Print("Compare() 1 Ok in NewGenRandom")
+	}
+	res = oUuid.Compare("24F4F1D8-E394-11E8-98E0-F0DEF1A82164")
+	if res == 0 {
+		t.Error("Error to Compare() 2 with uuid_parse in NewGenRandom")
+	}else{
+    log.Print("Compare() 2 Ok in NewGenRandom")
+  }
+	if oUuid.IsNull() {
+		t.Error("Error to check IsNull() 1 in NewGenRandom")
+	}else{
+    log.Print("IsNull() 1 Ok in NewGenRandom")
+  }
+	oUuid.Clear()
+	if ! oUuid.IsNull() {
+		t.Error("Error to check IsNull() 2 in NewGenRandom")
+	}else{
+    log.Print("Clear() and IsNull() 2 Ok in NewGenRandom")
+  }
+
+}
+
+func TestNewGen(t *testing.T) {
+	goUuid := NewGen()
+	defer goUuid.Close()
+	if goUuid.IsNull() {
+		t.Error("Error to initialize NewGen")
+	}
+	log.Printf("NewGen UnparseUpper:%s\n", goUuid.UnparseUpper())
+}
+
+func TestNewGenTime(t *testing.T) {
+	tUuid := NewGenTime()
+	defer tUuid.Close()
+	if tUuid.IsNull() {
+		t.Error("Error to initialize NewGenTime")
+	}
+	log.Printf("NewGenTime UnparseUpper:%s\n", tUuid.UnparseUpper())
+}
+
+func TestNewGenTimeSafe(t *testing.T) {
+	tsUuid := NewGenTimeSafe()
+	defer tsUuid.Close()
+	if tsUuid.IsNull() {
+		t.Error("Error to initialize NewGenTimeSafe")
+	}
+	log.Printf("NewGenTimeSafe UnparseUpper:%s\n", tsUuid.UnparseUpper())
+}
+
 func TestGetUUIDUpper(t *testing.T) {
 	uuid1 = GetUUIDUpper()
 	l := len(uuid1)
-	log.Printf("%s\n",uuid1)
+	log.Printf("GetUUIDUpper:%s\n",uuid1)
 	if l != 36 {
 		t.Errorf("uuid:%v:%d string is not correct",uuid1, l)
 	}
@@ -27,9 +98,31 @@ func TestGetUUIDUpper(t *testing.T) {
 func TestGetUUIDLower(t *testing.T) {
 	uuid2 = GetUUIDLower()
 	l := len(uuid2)
-	log.Printf("%s\n",uuid2)
+	log.Printf("GetUUIDLower:%s\n",uuid2)
 	if l != 36 {
 		t.Errorf("uuid:%v:%d string is not correct",uuid2,l)
+	}
+}
+
+func TestNewGenMd5(t *testing.T) {
+	u1 :="24f4f1d8-e394-11e8-98e0-f0def1a82164"
+	//u1 :="a8558cff-e3b1-4829-937d-77616766668e"
+	//u1 :="24F4F1D8-E394-11E8-98E0-F0DEF1A82164"
+	//u1 :="1B4E28BA-2FA1-11D2-883F-0016D3CCA427"
+	s1 :="golang.org"
+	//s1 :="tip.golang.org"
+	//s1 :="github.org"
+	mUuid := NewGenMd5(u1, s1)
+	defer mUuid.Close()
+	if mUuid.IsNull() {
+    t.Error("Error to initialize NewGenMd5")
+  }
+	log.Printf("NewGenMd5: %s\n", mUuid.UnparseLower())
+	log.Printf("NewGenMd5: %s\n", mUuid.UnparseUpper())
+	uuid2=mUuid.Val()
+	l := len(uuid2)
+	if l != 36 {
+		t.Errorf("NewGenMd5:%v:%d string is not correct",uuid2,l)
 	}
 }
 
@@ -55,6 +148,26 @@ func TestGetUUIDMd5Lower(t *testing.T) {
 	log.Printf("MD5:%s\n",uuid2)
 	if l != 36 {
 		t.Errorf("MD5:%v:%d string is not correct",uuid2,l)
+	}
+}
+
+func TestNewGenSha1(t *testing.T) {
+	//u1 :="24F4F1D8-E394-11E8-98E0-F0DEF1A82164"
+	u1 :="1B4E28BA-2FA1-11D2-883F-0016D3CCA427"
+	//s1 :="golang.org"
+	s1 :="github.org"
+	//oUuid = NewGen()
+	sUuid := NewGenSha1(u1, s1)
+	defer sUuid.Close()
+	if sUuid.IsNull() {
+    t.Error("Error to initialize NewGenSha1")
+  }
+	log.Printf("NewGenSha1:%s \n", sUuid.UnparseLower())
+	log.Printf("NewGenSha1:%s \n", sUuid.UnparseUpper())
+	uuid2=sUuid.Val()
+	l := len(sUuid.Val())
+	if l != 36 {
+		t.Errorf("NewGenSha1:%v:%d string is not correct",uuid2,l)
 	}
 }
 
@@ -86,25 +199,35 @@ func TestGetUUIDSha1Lower(t *testing.T) {
 
 func TestUUIDCompare(t *testing.T) {
 	res := UUIDCompare(uuid1, uuid1)
-	log.Printf("compare1:%d\n", res)
 	if res != 0 {
 		t.Error("UUIDCompare 1 is not correct")
+	}else{
+		log.Printf("compare1 Ok: %d\n", res)
 	}
 	res = UUIDCompare(uuid1, uuid2)
-	log.Printf("%s:%s:compare2:%d\n",uuid1, uuid2, res)
 	if res == 0 {
 		t.Error("UUIDCompare 2 is not correct")
+	}else{
+		log.Printf("compare2 Ok: %d\n",res)
 	}
 }
 
 const (
-  lower = iota
+	newgen = iota
+	newgenrandom
+	newgentime
+	newgentimesafe
+	newgensha1
+	newgenmd5
+  lower
   upper
 	md5l
 	md5u
 	sha1l
 	sha1u
 	gid
+	guuid4
+	guuid5
 )
 
 func benchmarkGetUUID(b *testing.B, mode int){
@@ -112,6 +235,24 @@ func benchmarkGetUUID(b *testing.B, mode int){
   b.ResetTimer()
   for i := 0; i < b.N; i++ {
 		switch mode {
+      case newgen:
+				oUuid = NewGen()
+				uuid1 = oUuid.UnparseUpper()
+      case newgenrandom:
+				oUuid = NewGenRandom()
+				uuid1 = oUuid.UnparseUpper()
+      case newgentime:
+				oUuid = NewGenTime()
+				uuid1 = oUuid.UnparseUpper()
+      case newgentimesafe:
+				oUuid = NewGenTimeSafe()
+				uuid1 = oUuid.UnparseUpper()
+      case newgensha1:
+				oUuid = NewGenSha1("1b4e28ba-2fa1-11d2-883f-0016d3cca427", "golang.org")
+				uuid1 = oUuid.UnparseUpper()
+      case newgenmd5:
+				oUuid = NewGenMd5("c0086038-e39e-11e8-98e0-f0def1a82164", "github.org")
+				uuid1 = oUuid.UnparseUpper()
       case lower:
 				uuid1 = GetUUIDLower()
       case upper:
@@ -127,11 +268,22 @@ func benchmarkGetUUID(b *testing.B, mode int){
       case gid:
 				v4gob := goid.NewV4UUID()
 				uuid1 = v4gob.String()
+      case guuid4:
+				gouuid.NewV4()
+				//uuid1 = v4guui.String()
+      case guuid5:
+				gouuid.NewV5(gouuid.NamespaceDNS, "www.example.com")
 			}
 	}
 	//log.Printf("mode %d, %s", mode, uuid1)
 }
 
+func BenchmarkNewGen(b *testing.B){ benchmarkGetUUID(b, newgen)}
+func BenchmarkNewGenRandom(b *testing.B){ benchmarkGetUUID(b, newgenrandom)}
+func BenchmarkNewGenTime(b *testing.B){ benchmarkGetUUID(b, newgentime)}
+func BenchmarkNewGenTimeSafe(b *testing.B){ benchmarkGetUUID(b, newgentimesafe)}
+func BenchmarkNewGenSha1(b *testing.B){ benchmarkGetUUID(b, newgensha1)}
+func BenchmarkNewGenMd5(b *testing.B){ benchmarkGetUUID(b, newgenmd5)}
 func BenchmarkGetUUIDUpper(b *testing.B){ benchmarkGetUUID(b, upper)}
 func BenchmarkGetUUIDLower(b *testing.B){ benchmarkGetUUID(b, lower)}
 func BenchmarkGetUUIDMd5Upper(b *testing.B){ benchmarkGetUUID(b, md5u)}
@@ -139,3 +291,5 @@ func BenchmarkGetUUIDMd5Lower(b *testing.B){ benchmarkGetUUID(b, md5l)}
 func BenchmarkGetUUIDSha1Upper(b *testing.B){ benchmarkGetUUID(b, sha1u)}
 func BenchmarkGetUUIDSha1Lower(b *testing.B){ benchmarkGetUUID(b, sha1l)}
 func BenchmarkGetGOID     (b *testing.B){ benchmarkGetUUID(b, gid)}
+func BenchmarkGetGOUUID4     (b *testing.B){ benchmarkGetUUID(b, guuid4)}
+func BenchmarkGetGOUUID5     (b *testing.B){ benchmarkGetUUID(b, guuid5)}
